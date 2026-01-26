54d1f5488f8356dfdec137b14a260f807b22cf3e
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-01-27 07:32:25 +0900

    [add]アクションの前提条件などに使うクラス(要検討)

---
+++ Still/Assets/Scripts/GOAP/Action/ActionSettings.cs
@@ -0,0 +1,26 @@
+﻿using Still.Enum.CompareType;
+using Still.Enum.WorldStates;
+using UnityEngine;
+namespace Still.GOAP.Action.Setting
+{
+    [System.Serializable]
+    public class HasConditionActions
+    {
+        public string WorldStateKey => _key.ToString();
+        public int Value => _value;
+        public CompareType CompareType => _compareType;
+
+        [SerializeField] private WorldStateType _key;
+        [SerializeField] private int _value;
+        [SerializeField] private CompareType _compareType;
+    }
+    [System.Serializable]
+    public class NoConditionActions
+    {
+        public string WorldStateKey => _key.ToString();
+        public int Value => _value;
+
+        [SerializeField] private WorldStateType _key;
+        [SerializeField] private int _value;
+    }
+}
