9a714c5e8ccd69746075eb6b6cd33ffa8a7601d7
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-01-27 07:05:56 +0900

    [add]アクションのインターフェース追加

---
+++ Still/Assets/Scripts/GOAP/Action/IAction.cs
@@ -0,0 +1,31 @@
+﻿using Still.GOAP.Agent;
+using System.Collections.Generic;
+namespace Still.GOAP.Action
+{
+    public interface IAction
+    {
+        Dictionary<string, Condition> Preconditions { get; }
+        Dictionary<string, int> Effects { get; }
+        int ActionCost { get; }
+
+        /// <summary>
+        /// アクション開始時に一度だけ呼ばれる（ターゲットの設定など）
+        /// </summary>
+        /// <param name="agent"></param>
+        void SetTarget(IAgentController agent);
+
+        /// <summary>
+        /// 毎フレーム呼ばれる。完了したら true を返す
+        /// </summary>
+        /// <param name="agent"></param>
+        /// <returns></returns>
+        bool Perform(IAgentController agent);
+
+        /// <summary>
+        /// 実行中に前提条件が維持されているか確認
+        /// </summary>
+        /// <param name="agent"></param>
+        /// <returns></returns>
+        bool Execute(IAgentController agent);
+    }
+}
