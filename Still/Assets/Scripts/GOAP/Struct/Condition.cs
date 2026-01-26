f9375868a9676ec0a7032e26abbd54240714205c
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-01-27 07:07:01 +0900

    [add]アクションなどの前提条件に必要な比較用の構造体

---
+++ Still/Assets/Scripts/GOAP/Struct/Condition.cs
@@ -0,0 +1,13 @@
+﻿using Still.Enum.CompareType;
+
+public struct Condition
+{
+    public int Value;
+    public CompareType Comparison;
+
+    public Condition(int value, CompareType comparison)
+    {
+        Value = value;
+        Comparison = comparison;
+    }
+}
