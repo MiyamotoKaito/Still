a38bab10f2734510aa7c7d3a5e4d2fb997031f90
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-01-27 07:30:40 +0900

    [add]比較用のEnumj

---
+++ Still/Assets/Scripts/GOAP/Enum/CompareType.cs
@@ -0,0 +1,12 @@
+﻿namespace Still.Enum.CompareType
+{
+    public enum CompareType
+    {
+        Equal,      // 等しい (==)
+        Less,       // より小さい (<)
+        Greater,    // より大きい (>)
+        NotEqual,   // 等しくない (!=)
+        LessEqual,  // 以下 (<=)
+        GreaterEqual // 以上 (>=)
+    }
+}
