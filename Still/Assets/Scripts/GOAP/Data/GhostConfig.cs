e77fc6a9f824f88df4cdf95d8edbec2c61b11bce
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-01-27 07:31:54 +0900

    [add]ゴールのコンフィグ

---
+++ Still/Assets/Scripts/GOAP/Data/GhostConfig.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+namespace Still.GOAP.Agent.Config
+{
+    /// <summary>
+    /// Ghostの設定
+    /// </summary>
+    [CreateAssetMenu(fileName = "GhostConfig", menuName = "Config/GOAP/GhostConfig")]
+    public class GhostConfig : ScriptableObject
+    {
+        [Header("デフォルトの設定")]
+        public float GhostMoveSpeed;
+        public float GhostDashSpeed;
+        public float GhostStopDistance;
+        public float GhostFovLength;
+
+        [Header("<color=cyan>ここから下はアクションごとの設定</color>")]
+
+        [Header("<color=yellow>ExploreAction</color>")]
+        public float Radius;
+        public float ReExploreDelay = 5f; // 探索完了から次までの休憩時間
+
+        [Header("<color=yellow>ExploreAction</color>")]
+        public float Duration;
+    }
+}
