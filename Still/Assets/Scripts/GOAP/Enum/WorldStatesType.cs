38fff33ae09c8e9e9cffa98c0ab13eeeedfe7d72
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-01-27 07:31:09 +0900

    [add]ワールドステートのEnum

---
+++ Still/Assets/Scripts/GOAP/Enum/WorldStatesType.cs
@@ -0,0 +1,19 @@
+﻿namespace Still.Enum.WorldStates
+{
+    public enum WorldStateType
+    {
+        // プレイヤー
+        PlayerVisible,
+        NearbyPlayer,
+
+        // エネミー
+        IsCatch,
+        IsChasing,
+        IsSearching,
+        EnemyPatrol,
+        DistanceFromGhost,
+
+        // イベント
+        EventCount,
+    }
+}
