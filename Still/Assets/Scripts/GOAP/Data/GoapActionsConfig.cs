c3298de6e10530d7a822bb042a5c03a87f60a6fc
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-01-27 07:31:43 +0900

    [add]アクションのコンフィグ

---
+++ Still/Assets/Scripts/GOAP/Data/GoapActionsConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+namespace Still.GOAP.Action.Config
+{
+    [CreateAssetMenu(fileName = "EnemyActionConfig", menuName = "Config/GOAP/Actions")]
+    public class GhostActionData : ScriptableObject
+    {
+        public List<IAction> GhostActions => _ghostActions;
+        [SerializeReference, SubclassSelector]
+        private List<IAction> _ghostActions;
+    }
+}
