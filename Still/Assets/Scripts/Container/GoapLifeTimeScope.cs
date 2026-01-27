1a5df032fa25befc6a758358bab90ee15226342a
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-01-27 14:38:30 +0900

    [add]登録するもの追加

--- Still/Assets/Scripts/Container/GoapLifeTimeScope.cs
+++ Still/Assets/Scripts/Container/GoapLifeTimeScope.cs
@@ -1,16 +1,32 @@
+using System.Collections.Generic;
+using Still.GOAP.Action;
+using Still.GOAP.Action.Config;
+using Still.GOAP.Agent.Config;
+using Still.GOAP.Goal.Config;
+using Still.GOAP.Planner.Executor;
+using Still.GOAP.WorldState;
+using Still.GOAP.WorldState.Config;
+using Still.GOAP.WorldState.Observer;
 using UnityEngine;
+using VContainer;
+using VContainer.Unity;
 
-public class GoapLifeTimeScope : MonoBehaviour
+public class GoapLifeTimeScope : LifetimeScope
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GhostConfig _agentConfig;
+    [SerializeField] private GhostActionData _actionData;
+    [SerializeField] private WorldStateConfig _worldStatesConfig;
+    [SerializeField] private List<SubGoalConfig> _subgoals;
 
-    // Update is called once per frame
-    void Update()
+    protected override void Configure(IContainerBuilder builder)
     {
+        builder.RegisterInstance(_agentConfig);
+        builder.RegisterInstance<List<IAction>>(_actionData.GhostActions);
+        builder.RegisterInstance<List<SubGoalConfig>>(_subgoals);
         
+        builder.Register<WorldStates>(Lifetime.Singleton)
+            .WithParameter("initializeStates", _worldStatesConfig.GetStates());
+        builder.Register<GoapExecutor>(Lifetime.Singleton);
+        builder.Register<WorldStatesObserver>(Lifetime.Singleton);
     }
 }
