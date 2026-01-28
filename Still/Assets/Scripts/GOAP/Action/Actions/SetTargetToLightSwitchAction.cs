7e8c6adc6984f56e6fe3ee32523e43a40a828ee0
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-01-29 05:42:44 +0900

    [add]アクション追加

---
+++ Still/Assets/Scripts/GOAP/Action/Actions/SetTargetToLightSwitchAction.cs
@@ -0,0 +1,27 @@
+﻿using Still.GOAP.Agent;
+using UnityEngine;
+namespace Still.GOAP.Action
+{
+    [System.Serializable]
+    public class SetTargetToLightSwitchAction : ActionBase
+    {
+        private LightSwitch _lightSwtich;
+        public override bool Perform(IAgentController agent)
+        {
+            if (_lightSwtich == null) return true;
+
+            if (agent.IsArrived)
+            {
+                Debug.Log(($"[Action] {nameof(SetTargetToLightSwitchAction)}: 目的地に到着しました。"));
+                return true;
+            }
+            return false;
+        }
+
+        public override void SetTarget(IAgentController agent)
+        {
+            _lightSwtich = GameObject.FindAnyObjectByType<LightSwitch>();
+            agent.SetMoveDestination(_lightSwtich.transform.position);
+        }
+    }
+}
---
+++ Still/Assets/Scripts/GOAP/Action/Actions/TurnOffLightAction.cs
@@ -0,0 +1,31 @@
+﻿using Still.GOAP.Agent;
+using UnityEngine;
+namespace Still.GOAP.Action
+{
+    [System.Serializable]
+    public class TurnOffLightAction : ActionBase
+    {
+        public override bool Perform(IAgentController agent)
+        {
+            if (_lightSwith == null) return true; // ターゲットがなければ即完了
+
+            // スイッチを切り替える（物理的な操作）
+            if (_lightSwith.IsOn) // 電気がついている場合のみオフにする
+            {
+                _lightSwith.SwitchToggle(); // スイッチをオフにする操作
+                Debug.Log($"[Action] {nameof(TurnOffLightAction)}: 電気スイッチをオフにしました。");
+            }
+            else
+            {
+                Debug.LogWarning($"[Action] {nameof(TurnOffLightAction)}: 電気がすでに消えています。");
+            }
+            return true; // アクションは完了
+        }
+
+        public override void SetTarget(IAgentController agent)
+        {
+            _lightSwith = GameObject.FindAnyObjectByType<LightSwitch>();
+        }
+        private LightSwitch _lightSwith;
+    }
+}
