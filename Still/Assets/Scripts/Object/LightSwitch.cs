5a2f02e3372790d461414ddd63515b64ce82ed0d
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-01-29 04:29:56 +0900

    [add]ライトのスイッチ

---
+++ Still/Assets/Scripts/Object/LightSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    public bool IsOn;
+    public event Action<bool> SwitchChanged;
+
+    [ContextMenu("スイッチ切り替え")]
+    public void SwitchToggle()
+    {
+        IsOn = IsOn = true ? false : true;
+        SwitchChanged.Invoke(IsOn);
+    }
+}
