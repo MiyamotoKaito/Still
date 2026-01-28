716aedfcbfd1b4c555e8a9b16cb8ebb4656cf3ba
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-01-29 04:33:44 +0900

    [add]ワールドステートのタイプ追加

--- Still/Assets/Scripts/GOAP/Enum/WorldStatesType.cs
+++ Still/Assets/Scripts/GOAP/Enum/WorldStatesType.cs
@@ -2,19 +2,24 @@
 {
     public enum WorldStateType
     {
-        // プレイヤー
+        // プレイヤー関連
         PlayerVisible,
         NearbyPlayer,
+        LastKnownPlayerLocationID,
 
-        // エネミー
+        // エネミー自身の状態
         IsCatch,
         IsChasing,
         IsSearching,
         EnemyPatrol,
         DistanceFromGhost,
+        IsAlerted,
 
-        // オブジェクト
+        // オブジェクト・環境の状態
         OnLighting,
+        OnLightSwitchIsBroken,
+        HeardSound,
+
         // イベント
         EventCount,
     }
