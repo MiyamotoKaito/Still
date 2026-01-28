b00920d459463c2290afc3b660149eb9da8e3aac
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-01-28 16:12:07 +0900

    [add]各アクション追加

---
+++ Still/Assets/Scripts/GOAP/Action/Actions/ChaseAction.cs
@@ -0,0 +1,32 @@
+using Still.Enum.WorldStates;
+using Still.GOAP.Agent;
+using Still.GOAP.Planner;
+namespace Still.GOAP.Action
+{
+    [System.Serializable]
+    public class ChaseAction : ActionBase
+    {
+        public override bool Execute(IAgentController agent)
+        {
+            if (Evalution.IsSatisfied(_worldStates.CurrentStates, Preconditions))
+            {
+                return true;
+            }
+            _worldStates.ModifyState(WorldStateType.IsChasing.ToString(), 0);
+            agent.SetSpeed(agent.Config.GhostMoveSpeed);
+            return false;
+        }
+
+        public override bool Perform(IAgentController agent)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SetTarget(IAgentController agent)
+        {
+            //_player = GameObject.FindAnyObjectByType<Player>();
+        }
+
+        //private Player _player;
+    }
+}
---
+++ Still/Assets/Scripts/GOAP/Action/Actions/SearchAction.cs
@@ -0,0 +1,25 @@
+using Still.GOAP.Agent;
+using UnityEngine;
+namespace Still.GOAP.Action
+{
+    [System.Serializable]
+    public class SearchAction : ActionBase
+    {
+        public override bool Perform(IAgentController agent)
+        {
+            _duration -= Time.deltaTime;
+
+            if (_duration <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override void SetTarget(IAgentController agent)
+        {
+            _duration = agent.Config.Duration;
+        }
+        private float _duration;
+    }
+}
