8818364c43dd33ff763c04022add83ddf951ce31
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-01-30 03:46:15 +0900

    [add]ターゲットをもとに動くアクション

---
+++ Still/Assets/Scripts/GOAP/Action/Actions/MoveAction.cs
@@ -0,0 +1,24 @@
+﻿using Still.GOAP.Agent;
+using UnityEngine;
+namespace Still.GOAP.Action
+{
+    [System.Serializable]
+    public class MoveAction : ActionBase
+    {
+        public override bool Perform(IAgentController agent)
+        {
+            if (agent.IsArrived)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override void SetTarget(IAgentController agent)
+        {
+            _targetPos = agent.TargetPos;
+            agent.SetMoveDestination(_targetPos);
+        }
+        private Vector3 _targetPos;
+    }
+}
