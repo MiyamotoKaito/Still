05b9cf9d6a9abafdd33052be5aac17b7716e48c4
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-02-01 23:08:56 +0900

    [update]変数と関数追加

--- Still/Assets/Scripts/Player/View/PlayerView.cs
+++ Still/Assets/Scripts/Player/View/PlayerView.cs
@@ -1,11 +1,67 @@
+﻿using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 namespace Still.Player.View
 {
-    public class PlayerView : MonoBehaviour, IPlayerController
+    public class PlayerView : MonoBehaviour, IPlayerController, IDisposable
     {
-        public void UpdatePosition(Vector3 pos)
+        public Vector2 CurrentMoveValue => _currentMoveValue;
+        public bool IsDash => _isDash;
+
+        private PlayerInputActions _actions;
+        private Rigidbody _rigidbody;
+        private Vector2 _currentMoveValue;
+        private bool _isDash;
+
+        private void Awake()
         {
-            this.transform.position = pos;
+            _actions = new PlayerInputActions();
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+        public void EnablePlayerInput()
+        {
+            _actions.Player.Move.performed += OnInputMove;
+            _actions.Player.Move.canceled += OnInputMove;
+            _actions.Player.Dash.performed += OnDashPerformed;
+            _actions.Player.Dash.canceled += OnDashPerformed;
+            _actions.Player.Enable();
+        }
+
+        public void DisablePlayerInput()
+        {
+            _actions.Player.Move.performed -= OnInputMove;
+            _actions.Player.Move.canceled -= OnInputMove;
+            _actions.Player.Dash.performed -= OnDashPerformed;
+            _actions.Player.Dash.canceled -= OnDashPerformed;
+            _actions.Player.Disable();
+        }
+
+        private void OnInputMove(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                _currentMoveValue = context.ReadValue<Vector2>();
+            else if (context.canceled)
+                _currentMoveValue = Vector2.zero;
+        }
+
+        private void OnDashPerformed(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                _isDash = true;
+            else if (context.canceled)
+                _isDash = false;
+        }
+
+        public void Move(Vector3 direction, float speed)
+        {
+            _rigidbody.linearVelocity = direction * speed;
+        }
+
+        public void Dispose()
+        {
+            DisablePlayerInput();
+            _actions.Dispose();
         }
     }
 }
