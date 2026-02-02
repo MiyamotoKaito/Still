56dbee6baba4104bfba204cc103003ae2cc3e42e
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-02-03 06:53:31 +0900

    [add]スタミナView

---
+++ Still/Assets/Scripts/Player/View/StaminaView.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+namespace Still.Player.View
+{
+    public class StaminaView : MonoBehaviour
+    {
+        private Image _staminaGauge;
+
+        private void Start()
+        {
+            _staminaGauge = GetComponent<Image>();
+        }
+
+        public void UpdateStamina(float value)
+        {
+            _staminaGauge.fillAmount = value / 100;
+            Debug.Log($"StaminaView: スタミナ更新 {value}");
+        }
+    }
+}
