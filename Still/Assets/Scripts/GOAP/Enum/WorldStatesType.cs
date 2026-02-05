5d025e00ffc5ae98a628bce0eb1254b6757d6c63
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-02-06 02:03:51 +0900

    [add]ノックアクションに関連するオブジェクトなどのスクリプト

--- Still/Assets/Scripts/GOAP/Enum/WorldStatesType.cs
+++ Still/Assets/Scripts/GOAP/Enum/WorldStatesType.cs
@@ -25,6 +25,8 @@
 
         // イベント
         EventCount,
+        InTheRoom,
         FearLevel,
+        Knock,
     }
 }
---
+++ Still/Assets/Scripts/Object/Room.cs
@@ -0,0 +1,35 @@
+﻿using Still.Enum.WorldStates;
+using Still.GOAP.WorldState;
+using Still.Player.View;
+using UnityEngine;
+using VContainer;
+namespace Still.Object
+{
+    public class Room : MonoBehaviour
+    {
+        public bool InTheRoom => _inTheRoom;
+        public GameObject Door => _door;
+
+        [SerializeField] private GameObject _door;
+        [Inject] WorldStates _worldStates;
+        private bool _inTheRoom;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent<PlayerView>(out var playerView))
+            {
+                _worldStates.ModifyState(WorldStateType.InTheRoom.ToString(), 1);
+                _inTheRoom = true;
+            }
+
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.TryGetComponent<PlayerView>(out var playerView))
+            {
+                _worldStates.ModifyState(WorldStateType.InTheRoom.ToString(), 0);
+                _inTheRoom = false;
+            }
+        }
+    }
+}
