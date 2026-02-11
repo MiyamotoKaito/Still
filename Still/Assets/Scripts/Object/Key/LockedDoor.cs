22e88627b6696942938424763381d8ca1f2fd559
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-02-11 17:17:02 +0900

    [add]鍵付きのドアView

---
+++ Still/Assets/Scripts/Object/Key/LockedDoor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Still.Object.Door.View
+{
+    public class LockedDoor : DoorView
+    {
+        public event Action OnDoorLocked;
+        private bool _isLocked = true;
+
+        public override void Interact()
+        {
+            OnDoorLocked?.Invoke();
+
+            if (_isLocked) return;
+
+            ToggleIsOpen();
+        }
+
+        public override void ShowUI()
+        {
+            if (_isLocked)
+            {
+                _text.gameObject.SetActive(true);
+                _text.text = "鍵が掛かっている";
+                return;
+            }
+            base.ShowUI();
+        }
+
+        public void GetKey(bool isLocked)
+        {
+            _isLocked = isLocked;
+        }
+    }
+}
