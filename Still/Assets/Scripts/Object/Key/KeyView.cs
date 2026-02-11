09e995e3ed64c631519274c988f3cb44ffab89b0
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-02-11 17:16:38 +0900

    [add]KeyのView

---
+++ Still/Assets/Scripts/Object/Key/KeyView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+namespace Still.Object.Key
+{
+    public class KeyView : MonoBehaviour, IInteractable
+    {
+        public event Action OnKeyCollected;
+
+        [SerializeField]
+        private TextMeshProUGUI _text;
+        public void HideUI()
+        {
+            _text.gameObject.SetActive(false);
+        }
+
+        public void Interact()
+        {
+            OnKeyCollected?.Invoke();
+            this.gameObject.SetActive(false);
+        }
+
+        public void ShowUI()
+        {
+            _text.gameObject.SetActive(true);
+        }
+    }
+}
