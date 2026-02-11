8a11ee5980454f0683f73c0c6ef3bed0d582a583
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-02-11 17:15:55 +0900

    [add]Keyのモデル

---
+++ Still/Assets/Scripts/Object/Key/KeyModel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+namespace Still.Object.Key.Model
+{
+    public class KeyModel
+    {
+        public bool IsCollected => _isCollected;
+
+        private bool _isCollected = false;
+
+        public void Collect()
+        {
+            _isCollected = true;
+            Debug.Log("KeyModel: 鍵が収集されました。");
+        }
+    }
+}
