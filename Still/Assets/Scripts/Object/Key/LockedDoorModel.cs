909d296aae813b5cda6d6d6aff65ee97770b7d5a
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-02-11 17:17:18 +0900

    [add]鍵付きドアのモデル

---
+++ Still/Assets/Scripts/Object/Key/LockedDoorModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Still.Object.Door.Model
+{
+    public class LockedDoorModel
+    {
+        public bool isLocked => _isLocked;
+        private bool _isLocked = true;
+
+        public void Unlock()
+        {
+            _isLocked = false;
+        }
+    }
+}
