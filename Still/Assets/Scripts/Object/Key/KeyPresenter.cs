f10eb8fd1ccb9a41437423ceac1b430285b8e3cd
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-02-11 17:16:12 +0900

    [add]Keyのプレゼンター

---
+++ Still/Assets/Scripts/Object/Key/KeyPresenter.cs
@@ -0,0 +1,23 @@
+﻿using Still.Object.Key.Model;
+using UnityEngine;
+namespace Still.Object.Key
+{
+    public class KeyPresenter
+    {
+        private KeyView _key;
+        private KeyModel _keyModel;
+        public KeyPresenter(KeyView key, KeyModel keyModel)
+        {
+            _key = key;
+            _keyModel = keyModel;
+
+            _key.OnKeyCollected += UnlockDoor;
+        }
+
+        private void UnlockDoor()
+        {
+            _keyModel.Collect();
+            Debug.Log("鍵が掛かっているドアを開けれるようになった");
+        }
+    }
+}
