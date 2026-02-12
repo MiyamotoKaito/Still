141a15127530aabdddcfe638bdd95c414a3c59ab
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-02-12 22:33:57 +0900

    [update]テレポート処理

--- Still/Assets/Scripts/GOAP/Action/Actions/TeleportAction.cs
+++ Still/Assets/Scripts/GOAP/Action/Actions/TeleportAction.cs
@@ -1,17 +1,30 @@
 ﻿using Still.GOAP.Action;
 using Still.GOAP.Agent;
 using UnityEngine;
-
+[System.Serializable]
 public class TeleportAction : ActionBase
 {
     public override bool Perform(IAgentController agent)
     {
-        throw new System.NotImplementedException();
+        if (_farthestPos == null) return false;
+
+        return true;
     }
 
     public override void SetTarget(IAgentController agent)
     {
-        
+        var pos = GameObject.FindObjectsByType<TeleportPos>(FindObjectsSortMode.None);
+        float dis = 500;
+        foreach (var p in pos)
+        {
+            var d = Vector3.Distance(agent.Position, p.transform.position);
+            
+            if (d < dis)
+            {
+                _farthestPos = p.transform.position;
+                dis = d;
+            }
+        }
     }
-    private Vector3 _teleportPosition;
+    private Vector3 _farthestPos;
 }
