89803448583c7a731b90568d88b0f25b90efbb58
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-02-14 04:10:56 +0900

    [add]Tvの制御

---
+++ Still/Assets/Scripts/Object/Tv/Tv.cs
@@ -0,0 +1,38 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Tv : MonoBehaviour, IInteractable
+{
+    [SerializeField]
+    private TextMeshProUGUI _text;
+    private Light _tvLight;
+    private bool _isOn = false;
+    private void Awake()
+    {
+        _tvLight = GetComponentInChildren<Light>();
+        _tvLight.enabled = false;
+    }
+    public void HideUI()
+    {
+        _text.gameObject.SetActive(false);
+    }
+
+    public void Interact()
+    {
+        if (!_isOn)
+        {
+            _tvLight.enabled = true;
+            _isOn = true;
+            _text.gameObject.SetActive(false);
+        }
+    }
+
+    public void ShowUI()
+    {
+        if (!_isOn)
+        {
+            _text.text = "Watch TV";
+            _text.gameObject.SetActive(true);
+        }
+    }
+}
