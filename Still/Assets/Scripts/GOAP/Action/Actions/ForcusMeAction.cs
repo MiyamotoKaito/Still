13ffbe0fc54478ed5e33c24ffdc41652d427260e
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-02-14 05:06:42 +0900

    [add]集中アクション

---
+++ Still/Assets/Scripts/GOAP/Action/Actions/ForcusMeAction.cs
@@ -0,0 +1,22 @@
+﻿using Still.Enum.WorldStates;
+using Still.GOAP.Agent;
+using UnityEngine;
+namespace Still.GOAP.Action.Actions
+{
+    [System.Serializable]
+    public class FocusMeAction : ActionBase
+    {
+        [SerializeReference, SubclassSelector]
+        private IEvent _focusMeEvent;
+        public override bool Perform(IAgentController agent)
+        {
+            return _focusMeEvent.IsFinished;
+        }
+
+        public override void SetTarget(IAgentController agent)
+        {
+            _focusMeEvent.Initialize();
+            _focusMeEvent.OnEvent();
+        }
+    }
+}
