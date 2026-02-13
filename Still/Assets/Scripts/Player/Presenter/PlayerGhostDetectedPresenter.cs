4f0ffcb1a8fdd25f4607686a790cb26e58ad8134
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-02-14 05:11:17 +0900

    [add]プレイヤーが幽霊を発見したときのプレゼンター

---
+++ Still/Assets/Scripts/Player/Presenter/PlayerGhostDetectedPresenter.cs
@@ -0,0 +1,52 @@
+﻿using Still.Enum.WorldStates;
+using Still.GOAP.Agent;
+using Still.GOAP.WorldState;
+using UnityEngine;
+namespace Still.Player.Presenter
+{
+    public class PlayerGhostDetectedPresenter
+    {
+        private readonly PlayerSensorView _playerSensorView;
+        private readonly WorldStates _worldStates;
+        private int _ghostDetectedValue;
+
+        public PlayerGhostDetectedPresenter(PlayerSensorView playerSensorView, WorldStates worldStates)
+        {
+            _playerSensorView = playerSensorView;
+            _worldStates = worldStates;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            _playerSensorView.OnHitDetected += HandleGhostDetected;
+            _playerSensorView.OnHitLost += HandleGhostLost;
+        }
+
+        private void HandleGhostDetected(RaycastHit hit)
+        {
+            if (hit.collider.TryGetComponent<GAgent>(out var agent))
+            {
+                if (_ghostDetectedValue == 1) return;
+
+                _ghostDetectedValue = 1;
+                _worldStates.ModifyState(WorldStateType.GhostDetected.ToString(), _ghostDetectedValue);
+                Debug.Log("ゴーストを検知した");
+            }
+        }
+
+        private void HandleGhostLost()
+        {
+            if (_ghostDetectedValue == 1)
+            {
+                _ghostDetectedValue = 0;
+                _worldStates.ModifyState(WorldStateType.GhostDetected.ToString(), _ghostDetectedValue);
+            }
+        }
+        public void Dispose()
+        {
+            _playerSensorView.OnHitDetected -= HandleGhostDetected;
+            _playerSensorView.OnHitLost -= HandleGhostLost;
+        }
+    }
+}
