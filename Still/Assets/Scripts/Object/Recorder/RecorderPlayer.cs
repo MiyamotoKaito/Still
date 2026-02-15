9b3a36d4622c0d809d60329e3b392f22ef0b410b
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-02-15 23:40:45 +0900

    [add]レコーダー

---
+++ Still/Assets/Scripts/Object/Recorder/RecorderPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class RecorderPlayer : MonoBehaviour,IInteractable
+{
+    public event Action OnInteract;
+    [SerializeField]
+    private TextMeshProUGUI _text;
+    private AudioSource _audiosource;
+    public void HideUI()
+    {
+        _text.gameObject.SetActive(false);
+    }
+
+    public void Interact()
+    {
+        OnInteract?.Invoke();
+        PlayMusic();
+        _text.gameObject.SetActive(false);
+    }
+
+    public void ShowUI()
+    {
+        _text.gameObject.SetActive(true);
+        _text.text = "Play Music";
+    }
+
+    private void PlayMusic()
+    {
+        AudioManager.Instance.PlayBGM("Recorder", _audiosource);
+    }
+}
