5b4287c84b95864199940ae8b20aaaeb71e521f1
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-02-16 02:55:48 +0900

    [update]スポーンのポジション変数追加

--- Still/Assets/Scripts/Object/Key/KeyView.cs
+++ Still/Assets/Scripts/Object/Key/KeyView.cs
@@ -6,9 +6,11 @@ namespace Still.Object.Key
     public class KeyView : MonoBehaviour, IInteractable
     {
         public event Action OnKeyCollected;
-
+        public GameObject[] SpawnPositions => _spawnPositions;
         [SerializeField]
         private TextMeshProUGUI _text;
+        [SerializeField]
+        private GameObject[] _spawnPositions;
         public void Init()
         {
             this.gameObject.SetActive(false);
@@ -34,6 +36,17 @@ namespace Still.Object.Key
             Debug.Log($"KeyView: {gameObject.name} ShowUI called."); // どのオブジェクトか判別
             _text.gameObject.SetActive(true);
             _text.text = "Collect Key";
+        }
+        public void Spawn()
+        {
+            if (_spawnPositions == null || _spawnPositions.Length == 0)
+            {
+                Debug.LogError($"{gameObject.name} の SpawnPositions が設定されていません！");
+                return;
+            }
+            int randomIndex = UnityEngine.Random.Range(0, _spawnPositions.Length);
+            this.transform.position = _spawnPositions[randomIndex].transform.position;
+            this.gameObject.SetActive(true);
         }
     }
 }
