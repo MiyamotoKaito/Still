8954850e5f24228f579a63198ecce191f37af17f
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-02-15 23:40:35 +0900

    [add]本

---
+++ Still/Assets/Scripts/Object/Book/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class Book : MonoBehaviour, IInteractable
+{
+    public event Action OnInteract;
+    [SerializeField] private TextMeshProUGUI _text;
+    [SerializeReference, SubclassSelector]
+    private IEvent _event;
+    public void HideUI()
+    {
+        _text.gameObject.SetActive(false);
+    }
+
+    public void Interact()
+    {
+        _text.gameObject.SetActive(false);
+        _event.Initialize();
+        OnInteract?.Invoke();
+        _event.OnEvent();
+    }
+
+    public void ShowUI()
+    {
+        _text.gameObject.SetActive(true);
+    }
+}
