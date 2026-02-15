47df0191757e1d6129ff9112120d38a47d9a17cb
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-02-15 23:40:22 +0900

    [add]本棚のライトを消すイベント

---
+++ Still/Assets/Scripts/Event/BookShellLightOff.cs
@@ -0,0 +1,41 @@
+﻿using Cysharp.Threading.Tasks;
+using Still.Player.View;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class BookShellLightOffEvent : IEvent
+{
+    public bool IsFinished => _isFinished;
+
+    [SerializeField]
+    private CinemachineCamera _playerCamera;
+    [SerializeField]
+    private CinemachineCamera _bookCam;
+    [SerializeField]
+    private Animator _bookShellAnimator;
+    private PlayerView _playerView;
+    private bool _isFinished = false;
+    public void Initialize()
+    {
+        _bookCam.transform.position = _playerCamera.transform.position;
+        _bookCam.Priority = 0;
+        _isFinished = false;
+        _playerView = GameObject.FindAnyObjectByType<PlayerView>();
+    }
+
+    public void OnEvent()
+    {
+        PlayAnimation().Forget();
+    }
+    private async UniTask PlayAnimation()
+    {
+        _playerView.DisablePlayerInput();
+        _bookCam.Priority = 10;
+        await UniTask.Delay(2000); // カメラ切り替えのために待機
+        _bookShellAnimator.SetTrigger("LightOff");
+        await UniTask.Delay(1000); // アニメーションの長さに合わせて待機
+        _playerView.EnablePlayerInput();
+        _bookCam.Priority = 0;
+        _isFinished = true;
+    }
+}
